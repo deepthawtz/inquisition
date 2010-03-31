6044d9d5da56a69ecdda478fbe6123e6f7bb61f9
Dylan Clendenin <dylan.clendenin@gmail.com>
2010-03-31 12:39:29 -0700

    helpful error messages concerning MongoDB setup phase. Added empty? method on collection.

--- setup.rb
+++ setup.rb
@@ -1,11 +1,24 @@
+begin
+  require "mongo"; include Mongo
+rescue LoadError
+  puts "You need the mongo gem\nTry: gem install mongo"
+  exit(1)
+end
 
-require "mongo"; include Mongo
+begin
+  db = Connection.new("localhost", Connection::DEFAULT_PORT).db("puravida")
+  Vocabulary = db.collection("vocab_quiz")
+rescue Mongo::ConnectionFailure
+  puts "Make sure MongoDB daemon (mongod) is running\nTry: mongod"
+  exit(1)
+end
 
-db = Connection.new("localhost", Connection::DEFAULT_PORT).db("puravida")
-Vocabulary = db.collection('vocab_quiz')
 
-
-# um, ghetto. but makes sense right?
 def Vocabulary.all
   Vocabulary.find.to_a
 end
+
+def Vocabulary.empty?
+  (Vocabulary.count == 0 ? true : false)
+end
+ 
